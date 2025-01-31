1a4cb26f8d6643bbd9e2b18cbd7bcdb1fe45cb39
komikodok <komikodok666@gmail.com>
2025-01-31 13:32:33 +0700

    minor update

--- groq_api_response.py
+++ groq_api_response.py
@@ -3,9 +3,7 @@ import os
 from dotenv import find_dotenv, load_dotenv
 
 
-load_dotenv(find_dotenv())
-print(f"load env: {load_dotenv()}")
-print(f"find env: {find_dotenv()}")
+load_dotenv(find_dotenv('.env.example'))
 
 groq_api = os.getenv('GROQ_API_KEY') # API key yg udah di copas dari groq diambil dari file .env
 llm = os.getenv('LLM') # llm yg mau dipilih cek semua model di https://console.groq.com/docs/models misal "gemma2-9b-it"
@@ -26,6 +24,6 @@ def get_response(user_input: str):
         }
     )
 
-    # result = response.json()["choices"][0]["message"]["content"] # output dari llm
+    result = response.json()["choices"][0]["message"]["content"] # output dari llm
 
-    return response.json()
+    return result
